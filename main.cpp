03494bc617a7bd7d1e7e8e3f14ca6395df467ed0
Scott Hanselman <scott@hanselman.com>
2026-01-09 17:21:38 -0800

    Add proper AUMID registration - notifications now work

--- main.cpp
+++ main.cpp
@@ -1,21 +1,35 @@
 #include <windows.h>
+#include <shobjidl.h>
+#include <propkey.h>
+#include <propvarutil.h>
+#include <shlobj.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Data.Xml.Dom.h>
 #include <winrt/Windows.UI.Notifications.h>
 #include <iostream>
 #include <string>
+#include <filesystem>
+
+#pragma comment(lib, "shlwapi.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "ole32.lib")
 
 using namespace winrt;
 using namespace Windows::Data::Xml::Dom;
 using namespace Windows::UI::Notifications;
+
+const wchar_t* APP_ID = L"Toasty.CLI.Notification";
+const wchar_t* APP_NAME = L"Toasty";
 
 void print_usage() {
     std::wcout << L"toasty - Windows toast notification CLI\n\n"
                << L"Usage: toasty <message> [options]\n\n"
                << L"Options:\n"
                << L"  -t, --title <text>   Set notification title (default: \"Notification\")\n"
-               << L"  -h, --help           Show this help\n\n"
+               << L"  -h, --help           Show this help\n"
+               << L"  --register           Register app for notifications (run once)\n\n"
                << L"Examples:\n"
+               << L"  toasty --register\n"
                << L"  toasty \"Build completed\"\n"
                << L"  toasty \"Task done\" -t \"Claude Code\"\n";
 }
@@ -36,6 +50,61 @@ std::wstring escape_xml(const std::wstring& text) {
     return result;
 }
 
+// Create a Start Menu shortcut with our AppUserModelId
+bool create_shortcut() {
+    wchar_t exePath[MAX_PATH];
+    GetModuleFileNameW(nullptr, exePath, MAX_PATH);
+
+    wchar_t startMenuPath[MAX_PATH];
+    if (FAILED(SHGetFolderPathW(nullptr, CSIDL_PROGRAMS, nullptr, 0, startMenuPath))) {
+        return false;
+    }
+
+    std::wstring shortcutPath = std::wstring(startMenuPath) + L"\\Toasty.lnk";
+
+    CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
+    IShellLinkW* shellLink = nullptr;
+    HRESULT hr = CoCreateInstance(CLSID_ShellLink, nullptr, CLSCTX_INPROC_SERVER,
+                                   IID_IShellLinkW, (void**)&shellLink);
+    if (FAILED(hr)) {
+        CoUninitialize();
+        return false;
+    }
+
+    shellLink->SetPath(exePath);
+    shellLink->SetDescription(L"Toasty - Toast Notification CLI");
+
+    IPropertyStore* propStore = nullptr;
+    hr = shellLink->QueryInterface(IID_IPropertyStore, (void**)&propStore);
+    if (SUCCEEDED(hr)) {
+        PROPVARIANT pv;
+        hr = InitPropVariantFromString(APP_ID, &pv);
+        if (SUCCEEDED(hr)) {
+            propStore->SetValue(PKEY_AppUserModel_ID, pv);
+            PropVariantClear(&pv);
+        }
+        propStore->Commit();
+        propStore->Release();
+    }
+
+    IPersistFile* persistFile = nullptr;
+    hr = shellLink->QueryInterface(IID_IPersistFile, (void**)&persistFile);
+    if (SUCCEEDED(hr)) {
+        hr = persistFile->Save(shortcutPath.c_str(), TRUE);
+        persistFile->Release();
+    }
+
+    shellLink->Release();
+    CoUninitialize();
+
+    if (SUCCEEDED(hr)) {
+        std::wcout << L"Registered! Shortcut created at:\n" << shortcutPath << L"\n";
+        return true;
+    }
+    return false;
+}
+
 int wmain(int argc, wchar_t* argv[]) {
     if (argc < 2) {
         print_usage();
@@ -44,6 +113,7 @@ int wmain(int argc, wchar_t* argv[]) {
 
     std::wstring message;
     std::wstring title = L"Notification";
+    bool doRegister = false;
 
     for (int i = 1; i < argc; i++) {
         std::wstring arg = argv[i];
@@ -52,6 +122,9 @@ int wmain(int argc, wchar_t* argv[]) {
             print_usage();
             return 0;
         }
+        else if (arg == L"--register") {
+            doRegister = true;
+        }
         else if (arg == L"-t" || arg == L"--title") {
             if (i + 1 < argc) {
                 title = argv[++i];
@@ -59,6 +132,16 @@ int wmain(int argc, wchar_t* argv[]) {
         }
         else if (arg[0] != L'-' && message.empty()) {
             message = arg;
+        }
+    }
+
+    if (doRegister) {
+        if (create_shortcut()) {
+            std::wcout << L"App registered for notifications.\n";
+            return 0;
+        } else {
+            std::wcerr << L"Failed to register app.\n";
+            return 1;
         }
     }
 
@@ -71,6 +154,9 @@ int wmain(int argc, wchar_t* argv[]) {
     try {
         init_apartment();
 
+        // Set our AppUserModelId for this process
+        SetCurrentProcessExplicitAppUserModelID(APP_ID);
+
         std::wstring xml = L"<toast><visual><binding template=\"ToastGeneric\">"
                           L"<text>" + escape_xml(title) + L"</text>"
                           L"<text>" + escape_xml(message) + L"</text>"
@@ -81,8 +167,7 @@ int wmain(int argc, wchar_t* argv[]) {
 
         ToastNotification toast(doc);
 
-        // Use PowerShell's AppUserModelId for unpackaged apps
-        auto notifier = ToastNotificationManager::CreateToastNotifier(L"Microsoft.Windows.PowerShell");
+        auto notifier = ToastNotificationManager::CreateToastNotifier(APP_ID);
         notifier.Show(toast);
 
         return 0;
